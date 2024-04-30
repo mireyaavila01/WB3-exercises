ee9ab25e1d91217dd1c2161a879cf83850cfe005
Mireya Avila <mireyaavila147@yahoo.com>
2024-04-30 14:00:45 -0400

    completed exercise 1-6

---
+++ Functions/simple_functions.js
@@ -0,0 +1,21 @@
+"use strict"
+
+function favoriteThings(){
+    console.log("Mireya");
+    console.log("Fast and Furious");
+    console.log("Bad Bunny");
+
+}
+
+function whyImHere(){
+
+    console.log("I joined the program to help me be successful in life.");
+}
+
+function favoritePlace(){
+    console.log("One of my favorite places to visit would be the beach because I love the environment.");
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
