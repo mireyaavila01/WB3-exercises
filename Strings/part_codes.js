f62881151bbab5f9f474b1b67c68d585d0c73c8e
Mireya Avila <mireyaavila147@yahoo.com>
2024-05-02 09:24:15 -0400

    completed part_codes.js

--- Strings/part_codes.js
+++ Strings/part_codes.js
@@ -1,25 +1,32 @@
 "use strict"
 
-let partCode = "MYA:12345-L"
+
+let partCode = "ABCD:123-S"
+let colon = partCode.indexOf(":");
+let dash = partCode.indexOf("-");
+
+//calculations
 
 function getSupplier(partCode){
-
-    let partSupplier = partCode.substring(0, ":");
+    
+    let partSupplier = partCode.substring(0, colon);
     console.log("the supplier is " + partSupplier);
 }
 
 function getProductNumber(partCode){
 
-    let productNumber = partCode.substring(4, 9);
+    let productNumber = partCode.substring(colon +1 , dash);
     console.log("the product number is " + productNumber);
 }
 
 function getSize(partCode){
-
-    let partSize = partCode.substring(10);
+    
+    let partSize = partCode.substring(dash + 1);
     console.log("the part size " + partSize);
 
 }
+
+//call the results
 
 getSupplier(partCode);
 getProductNumber(partCode);
